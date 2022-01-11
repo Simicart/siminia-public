8316a49cb7c79177d5bda05fdbbcfc6cf5c9487a
Cody Nguyen <cody@simicart.com>
2022-01-11 12:00:06 +0700

    Remove unused func from identify

--- src/simi/Helper/Identify.js
+++ src/simi/Helper/Identify.js
@@ -1,6 +1,5 @@
 import * as Constants from 'src/simi/Config/Constants';
 import CacheHelper from 'src/simi/Helper/CacheHelper';
-import isObjectEmpty from 'src/util/isObjectEmpty';
 
 import { CACHE_PERSIST_PREFIX } from '@magento/peregrine/lib/Apollo/constants';
 
@@ -102,22 +101,6 @@ class Identify {
         sessionStorage.removeItem('LOCAL_URL_DICT');
         sessionStorage.removeItem(Constants.STORE_CONFIG);
     }
-
-    static isEnabledCheckoutAsGuest() {
-        const data = this.getStoreConfig();
-        if (
-            data &&
-            data.simiStoreConfig &&
-            data.simiStoreConfig.config &&
-            data.simiStoreConfig.config.checkout &&
-            data.simiStoreConfig.config.checkout.enable_guest_checkout
-        ) {
-            return !!parseInt(
-                data.simiStoreConfig.config.checkout.enable_guest_checkout
-            );
-        }
-        return false;
-    }
     /*
     Dashboard config handlers
     */
@@ -197,137 +180,6 @@ class Identify {
             return data;
         }
         console.log('This browser does not support local storage');
-    }
-
-    static ApiDataStorage(key = '', type = 'get', data = {}) {
-        let api_data = this.getDataFromStoreage(this.SESSION_STOREAGE, key);
-        if (type === 'get') {
-            return api_data;
-        } else if (type === 'update' && data) {
-            api_data = api_data ? api_data : {};
-            api_data = { ...api_data, ...data };
-            this.storeDataToStoreage(this.SESSION_STOREAGE, key, api_data);
-        }
-    }
-
-    /*
-    Version control
-    */
-    //version control
-    static detectPlatforms() {
-        if (navigator.userAgent.match(/iPad|iPhone|iPod/)) {
-            return 1;
-        } else if (navigator.userAgent.match(/Android/)) {
-            return 2;
-        } else {
-            return 3;
-        }
-    }
-
-    static validateEmail(email) {
-        return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
-    }
-
-    static PadWithZeroes(number, length) {
-        let my_string = '' + number;
-        while (my_string.length < length) {
-            my_string = '0' + my_string;
-        }
-
-        return my_string;
-    }
-
-    static formatAddress(address = {}, countries = []) {
-        const country = countries.find(({ id }) => id === address.country_id);
-        const { available_regions: regions } = country;
-        if (!regions || (regions instanceof Array && regions.length < 1)) {
-            if (
-                address.hasOwnProperty('region') &&
-                address.region instanceof Object &&
-                Object.keys(address.region).length
-            ) {
-                const regionUnavailable = address.region;
-                const addRtn = { ...address };
-                if (regionUnavailable.hasOwnProperty('region_code')) {
-                    addRtn.region_code = regionUnavailable.region_code;
-                }
-                if (regionUnavailable.hasOwnProperty('region_id')) {
-                    addRtn.region_id = regionUnavailable.region_id;
-                }
-                if (regionUnavailable.hasOwnProperty('region')) {
-                    addRtn.region = regionUnavailable.region;
-                }
-                return addRtn;
-            }
-            return address;
-        } else {
-            let region = {};
-            if (address.hasOwnProperty('region_code')) {
-                const { region_code } = address;
-                region = regions.find(({ code }) => code === region_code);
-            } else if (
-                address.hasOwnProperty('region') &&
-                !isObjectEmpty(address.region)
-            ) {
-                const region_list = address.region;
-                const { region_code } = region_list;
-                if (region_code) {
-                    region = regions.find(({ code }) => code === region_code);
-                } else {
-                    region = {
-                        region: 'Mississippi',
-                        region_code: 'MS',
-                        region_id: 35
-                    };
-                }
-            } else {
-                //fake region to accept current shipping address
-                region = {
-                    region: 'Mississippi',
-                    region_code: 'MS',
-                    region_id: 35
-                };
-            }
-
-            return {
-                ...address,
-                country_id: address.country_id,
-                region_id: parseInt(region.id, 10),
-                region_code: region.code,
-                region: region.name
-            };
-        }
-
-        /* let region = {};
-        if (regions) {
-            region = regions.find(({ code }) => code === region_code);
-        } else {
-            //fake region to accept current shipping address
-            region = { region: "Mississippi", region_code: "MS", region_id: 35 };
-        }
-        return {
-            ...address,
-            country_id: address.country_id,
-            region_id: parseInt(region.id, 10),
-            region_code: region.code,
-            region: region.name
-        }; */
-    }
-
-    static formatDate(dateInput) {
-        let newDate = new Date(dateInput);
-        if (window.navigator && !navigator.userAgent.match(/Android/)) {
-            const arr = dateInput.split(/[- :]/);
-            newDate = new Date(
-                arr[0],
-                arr[1] - 1,
-                arr[2],
-                arr[3],
-                arr[4],
-                arr[5]
-            );
-        }
-        return newDate;
     }
 }
 
