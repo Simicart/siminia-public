58d057164cb8b89bdfb88fb14d33160c7cf892e5
nnduy1998 <it.nnduy09@gmail.com>
2022-01-19 16:52:28 +0700

    fix store config data

--- src/simi/App/core/Seo/queries/getStoreConfigData.gql.js
+++ src/simi/App/core/Seo/queries/getStoreConfigData.gql.js
@@ -1,8 +1,14 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
+const mageworxSeoEnabled =
+    window.SMCONFIGS &&
+    window.SMCONFIGS.plugins &&
+    window.SMCONFIGS.plugins.SM_ENABLE_MAGEWORX_SEO &&
+    parseInt(window.SMCONFIGS.plugins.SM_ENABLE_MAGEWORX_SEO) === 1;
+
 export const GET_STORE_CONFIG_DATA = gql`
 query storeConfigData {
     storeConfig {
-        mageworx_seo
+        ${mageworxSeoEnabled ? `mageworx_seo` : ``}
     }
     currency {
         default_display_currency_code
