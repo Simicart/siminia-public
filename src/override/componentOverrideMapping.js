ac58c57e64a6aaec40087954a718bbefcaa89212
Bach Van Hieu <bachvanhieu@MacBook-Air-cua-Bach.local>
2022-01-25 15:06:35 +0700

    temp

--- src/override/componentOverrideMapping.js
+++ src/override/componentOverrideMapping.js
@@ -10,5 +10,6 @@ module.exports = componentOverride = {
     [`@magento/peregrine/lib/talons/Header/useCurrencySwitcher.js`]: '@simicart/siminia/src/simi/talons/Header/useCurrencySwitcher.js',
     [`@magento/peregrine/lib/talons/MegaMenu/useMegaMenu.js`]: '@simicart/siminia/src/override/useMegaMenu.js',
     [`@magento/venia-ui/lib/components/Button/button.module.css`]: '@simicart/siminia/src/override/button.module.css',
-    [`@magento/peregrine/lib/util/deriveErrorMessage.js`]: '@simicart/siminia/src/override/deriveErrorMessage.js'
+    [`@magento/peregrine/lib/util/deriveErrorMessage.js`]: '@simicart/siminia/src/override/deriveErrorMessage.js',
+    // [`@magento/venia-ui/lib/components/CheckoutPage/ShippingInformation/AddressForm/guestForm.js`] : '@simicart/siminia/src/override/guestForm.js'
 };
