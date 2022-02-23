860b7f4c3e07da85dcc89608b67db534d518bed2
Bach Van Hieu <bachvanhieu@MacBook-Air-cua-Bach.local>
2022-02-23 17:01:09 +0700

    fix product detail - native

--- src/simi/App/nativeInner/ProductFullDetail/addToCartPopup.js
+++ src/simi/App/nativeInner/ProductFullDetail/addToCartPopup.js
@@ -1,29 +1,43 @@
-import React, { Fragment, Suspense, useRef, useState } from 'react';
+import React, { Fragment, Suspense, useRef, useState, useEffect } from 'react';
 import { Form } from 'informed';
-import { GrClose } from "react-icons/gr";
+import { GrClose } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 
 require('./statusBar.scss');
 
 const AddToCartPopup = props => {
-    const { options, handleAddToCart, wrapperQuantity, cartAction, setAddToCartPopup,handleBuyNow, typeBtn } = props;
+    const {
+        options,
+        handleAddToCart,
+        wrapperQuantity,
+        cartAction,
+        setAddToCartPopup,
+        handleBuyNow,
+        typeBtn
+    } = props;
 
-    // const test = () => {
-    //     window.location.pathname = ('/cart')
-    // }
-
+  
 
     return (
         <div className="main-AddToCartPopup">
-            <div className="modal" />
+            <div  className="modal" />
             
-            <Form className='form' onSubmit={typeBtn === "add to cart" ? handleAddToCart : handleBuyNow}>
-            <GrClose className="close-icon" onClick={()=> setAddToCartPopup(false)} />
-                {options}
-                {wrapperQuantity}
-                {cartAction}
-                
-            </Form>
+                <Form
+                    className="form"
+                    onSubmit={
+                        typeBtn === 'add to cart'
+                            ? handleAddToCart
+                            : handleBuyNow
+                    }
+                >
+                    <GrClose
+                        className="close-icon"
+                        onClick={() => setAddToCartPopup(false)}
+                    />
+                    {options}
+                    {wrapperQuantity}
+                    {cartAction}
+                </Form>
             
         </div>
     );
