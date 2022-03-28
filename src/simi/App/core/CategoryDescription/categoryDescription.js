2370930f063110a4f0f2ec51537f55251411ece8
Duy <it.nnduy09@gmail.com>
2022-03-28 22:20:39 +0700

    Update button color for mini cart, add cms block

---
+++ src/simi/App/core/CategoryDescription/categoryDescription.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from 'react-dom';
+require('./categoryDescription.scss');
+import RichContent  from '@magento/venia-ui/lib/components/RichContent';
+const CategoryDescription = props => {
+    const childCate = props.childCate || {};
+    const description =
+        childCate && childCate.description ? childCate.description : '';
+    const cms_block =
+        childCate && childCate.cms_block ? childCate.cms_block : null;
+    const renderBlock = () => {
+        if (cms_block) {
+            return (
+                <div className={`${description ? '' : 'paddingTop'} wrapBlock`}>
+                    <div className="titleBlock">{cms_block.title}</div>
+                    <div className="content">
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: cms_block.content
+                            }}
+                        />
+                    </div>
+                </div>
+            );
+        }
+        return '';
+    };
+    return (
+        <div style={{marginBottom:'15px'}}>
+            <div className={`${description ? 'wrapRichContent' : ''}`}>
+                <RichContent html={description} />
+            </div>
+            {renderBlock()}
+        </div>
+    );
+};
+
+export default CategoryDescription;
