21d04cf33498f0a4c8e2d5be3ebb252fa33c8fc6
hung <quochung@simicart.com>
2022-03-28 17:58:38 +0700

    update cart button

--- src/simi/App/nativeInner/Cart/RectButton/RectButton.js
+++ src/simi/App/nativeInner/Cart/RectButton/RectButton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import defaultClass from './RectButton.module.css'
 import Button from "@magento/venia-ui/lib/components/Button/button";
-import {configColor} from "../../../../Config";
 
 // red button, but is squared
 export const RectButton = (props) => {
@@ -11,10 +10,6 @@ export const RectButton = (props) => {
         <Button {...restProps}
                 classes={defaultClass}
                 type={'submit'}
-                style={{
-                    backgroundColor: configColor.button_background,
-                    color: configColor.button_text_color
-                }}
         >
             {children}
         </Button>
