34d55be7620750c505db3d192b0a0285453a65c2
Duy <it.nnduy09@gmail.com>
2022-03-31 21:27:22 +0700

    Update no products found, sort by, filter

--- src/simi/App/nativeInner/Products/NoProductsFound/noProductsFound.js
+++ src/simi/App/nativeInner/Products/NoProductsFound/noProductsFound.js
@@ -6,18 +6,36 @@ import defaultClasses from './noProductsFound.module.css';
 import { useIntl } from 'react-intl';
 
 // TODO: get categoryUrlSuffix from graphql storeOptions when it is ready
-import noProducts from '../Icon/noProducts.png'
+import noProducts from '../Icon/noProducts.png';
+import noFilters from '../Icon/noFilters.png';
+
 const NoProductsFound = props => {
-    const {search} = props;
+    const { search } = props;
+    console.log('props', props);
     const classes = mergeClasses(defaultClasses, props.classes);
     const { formatMessage } = useIntl();
 
     return (
         <div className={classes.root}>
-            <img className={classes.imgNoProducts} src={noProducts} alt='noProducts'/>
+            {search ? (
+                <img
+                    className={classes.imgNoProducts}
+                    src={noProducts}
+                    alt="noProducts"
+                />
+            ) : (
+                <img
+                    className={classes.imgNoProducts}
+                    src={noFilters}
+                    alt="noFilters"
+                />
+            )}
             <div className={classes.noProducts}>
-                {formatMessage({ id: 'noProducts', defaultMessage: 'There are no products maching '})}
-                <span className={classes.search}>{`"${search}"`}</span>
+                {formatMessage({
+                    id: 'noProducts',
+                    defaultMessage: 'There are no products matching '
+                })}
+                {search ? <span className={classes.search}>{`"${search}"`}</span> : ''}
             </div>
         </div>
     );
