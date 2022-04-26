8c3c9ac09d12e9e767b7fd0cd3696b9d5ebd0471
Duy <it.nnduy09@gmail.com>
2022-04-26 07:55:08 +0700

    Update format message

--- src/simi/App/nativeInner/Faq/Article/components/AdditionalInfo/Rating.js
+++ src/simi/App/nativeInner/Faq/Article/components/AdditionalInfo/Rating.js
@@ -1,145 +1,194 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { ADD_HELPFUL } from '../../../../talons/Faq/Faq.gql'
-import * as Icon from 'react-feather'
-import defaultClasses from './Rating.module.css'
+import { ADD_HELPFUL } from '../../../../talons/Faq/Faq.gql';
+import * as Icon from 'react-feather';
+import defaultClasses from './Rating.module.css';
+import { useIntl } from 'react-intl';
 
 const Rating = props => {
-	const { id, positives, negatives } = props
-	const ratingData = JSON.parse(localStorage.getItem(`article${id}`))
-	let hasRatingData = ratingData ? true : false
-	const classes = defaultClasses
-	const [positive, setPositive] = useState(
-		!hasRatingData ? positives : (positives < ratingData.positive) ? ratingData.positive : positives
-	);
-	const [negative, setNegative] = useState(
-		!hasRatingData ? negatives : (negatives < ratingData.negative) ? ratingData.negative : negatives
-	);
-	const [positiveRated, setPositiveRated] = useState(
-		!hasRatingData ? false : (false || ratingData.positiveRated)
-	);
-	const [negativeRated, setNegativeRated] = useState(
-		!hasRatingData ? false : (false ||  ratingData.negativeRated)
-	);
-	const [disable, setDisable] = useState(
-		!hasRatingData ? false : (false || ratingData.disable)
-	)
-	const [display, setDisplay] = useState(
-		!hasRatingData ? false : (false || ratingData.display)
-	)
-	const [isRated, {data}] = useMutation(ADD_HELPFUL);
+    const { id, positives, negatives } = props;
+    const { formatMessage } = useIntl();
 
-	const messageStyle = {
-		background: !disable ? '#e5efe5' : '#fae5e5',
-		color: !disable ? '#006400' : '#e02b27',
-	}
-	return (
-		<div className={classes.container_rating}>
-			<div className={classes.rating_label}>
-				<span>Did you find it helpful? </span>
-				<div className={classes.rating_action}>
-					<div className={classes.positive} >
-						<button 
-							className={classes.btn}
-							style={{
-								'backgroundColor': positiveRated ? '#1abc9c' : '',
-								'color': positiveRated ? '#fff' : '#000'
-							}}
-							onClick={() => {
-								if(!positiveRated && !negativeRated) {
-									isRated({variables: {isHelpful: true, articleId: id}})
-										.then(res => localStorage.setItem(
-														`article${props.id}`, 
-														JSON.stringify({
-															id,
-															positive: positive+1,
-															negative,
-															positiveRated: true,
-															negativeRated,
-															disable,
-															display
-														})
-										)
-									)
-									setPositive(positive+1);
-									setPositiveRated(true)
-									setDisplay(true);
-									localStorage.setItem(
-										`article${props.id}`, 
-										JSON.stringify({
-											id,
-											positive: positive+1,
-											negative,
-											positiveRated: true,
-											negativeRated,
-											disable,
-											display
-										})
-									)
-								} else {
-									setDisplay(true)
-									setDisable(true)
-								}
-							}}
-						>
-							Yes&thinsp;
-							(<span>{positive}</span>)
-						</button>
-					</div>
-					<div className={classes.negative}>
-						<button 
-							className={classes.btn}
-							style={{
-								'backgroundColor': negativeRated ? '#1abc9c' : '',
-								'color': negativeRated ? '#fff' : '#000'
-							}}
-							onClick={() => {
-								if(!negativeRated && !positiveRated) {
-									isRated({variables: {isHelpful: false, articleId: id}})
-										.then(res => localStorage.setItem(
-														`article${props.id}`, 
-														JSON.stringify({
-															id,
-															positive,
-															negative: negative+1,
-															positiveRated,
-															negativeRated: true,
-															disable,
-															display
-														})
-										)
-									)
-									setNegative(negative+1);
-									setNegativeRated(true)
-									setDisplay(true)
-								} else {
-									setDisplay(true)
-									setDisable(true)
+    const ratingData = JSON.parse(localStorage.getItem(`article${id}`));
+    let hasRatingData = ratingData ? true : false;
+    const classes = defaultClasses;
+    const [positive, setPositive] = useState(
+        !hasRatingData
+            ? positives
+            : positives < ratingData.positive
+            ? ratingData.positive
+            : positives
+    );
+    const [negative, setNegative] = useState(
+        !hasRatingData
+            ? negatives
+            : negatives < ratingData.negative
+            ? ratingData.negative
+            : negatives
+    );
+    const [positiveRated, setPositiveRated] = useState(
+        !hasRatingData ? false : false || ratingData.positiveRated
+    );
+    const [negativeRated, setNegativeRated] = useState(
+        !hasRatingData ? false : false || ratingData.negativeRated
+    );
+    const [disable, setDisable] = useState(
+        !hasRatingData ? false : false || ratingData.disable
+    );
+    const [display, setDisplay] = useState(
+        !hasRatingData ? false : false || ratingData.display
+    );
+    const [isRated, { data }] = useMutation(ADD_HELPFUL);
 
-								}
-							}}
-						>
-							No&thinsp;
-							(<span>{negative}</span>)
-						</button>
-					</div>
-				</div>
-				&thinsp;
-			</div>
-			<div 
-				className={classes.message} 
-				style = {{
-					display: display ? 'block' : 'none',
-					...messageStyle
-				}}
-			>
-				{!disable ? <Icon.Check className={classes['message-icon']}/> : <Icon.X className={classes['message-icon']} />} 
-				<div>
-					{!disable ? 'Thank for your vote!' : 'You have voted already!'}
-				</div>
-			</div>
-		</div>
-	)
-}
+    const messageStyle = {
+        background: !disable ? '#e5efe5' : '#fae5e5',
+        color: !disable ? '#006400' : '#e02b27'
+    };
+    return (
+        <div className={classes.container_rating}>
+            <div className={classes.rating_label}>
+                <span>
+                    {formatMessage({
+                        id: 'helpFull',
+                        defaultMessage: 'Did you find it helpful?'
+                    })}
+                </span>
+                <div className={classes.rating_action}>
+                    <div className={classes.positive}>
+                        <button
+                            className={classes.btn}
+                            style={{
+                                backgroundColor: positiveRated ? '#1abc9c' : '',
+                                color: positiveRated ? '#fff' : '#000'
+                            }}
+                            onClick={() => {
+                                if (!positiveRated && !negativeRated) {
+                                    isRated({
+                                        variables: {
+                                            isHelpful: true,
+                                            articleId: id
+                                        }
+                                    }).then(res =>
+                                        localStorage.setItem(
+                                            `article${props.id}`,
+                                            JSON.stringify({
+                                                id,
+                                                positive: positive + 1,
+                                                negative,
+                                                positiveRated: true,
+                                                negativeRated,
+                                                disable,
+                                                display
+                                            })
+                                        )
+                                    );
+                                    setPositive(positive + 1);
+                                    setPositiveRated(true);
+                                    setDisplay(true);
+                                    localStorage.setItem(
+                                        `article${props.id}`,
+                                        JSON.stringify({
+                                            id,
+                                            positive: positive + 1,
+                                            negative,
+                                            positiveRated: true,
+                                            negativeRated,
+                                            disable,
+                                            display
+                                        })
+                                    );
+                                } else {
+                                    setDisplay(true);
+                                    setDisable(true);
+                                }
+                            }}
+                        >
+                            {formatMessage({
+                                id: 'yes',
+                                defaultMessage: 'Yes'
+                            })}
+                            &thinsp; (<span>{positive}</span>)
+                        </button>
+                    </div>
+                    <div className={classes.negative}>
+                        <button
+                            className={classes.btn}
+                            style={{
+                                backgroundColor: negativeRated ? '#1abc9c' : '',
+                                color: negativeRated ? '#fff' : '#000'
+                            }}
+                            onClick={() => {
+                                if (!negativeRated && !positiveRated) {
+                                    isRated({
+                                        variables: {
+                                            isHelpful: false,
+                                            articleId: id
+                                        }
+                                    }).then(res =>
+                                        localStorage.setItem(
+                                            `article${props.id}`,
+                                            JSON.stringify({
+                                                id,
+                                                positive,
+                                                negative: negative + 1,
+                                                positiveRated,
+                                                negativeRated: true,
+                                                disable,
+                                                display
+                                            })
+                                        )
+                                    );
+                                    setNegative(negative + 1);
+                                    setNegativeRated(true);
+                                    setDisplay(true);
+                                } else {
+                                    setDisplay(true);
+                                    setDisable(true);
+                                }
+                            }}
+                        >
+                            {formatMessage({
+                                id: 'no',
+                                defaultMessage: 'No'
+                            })}
+                            &thinsp; (<span>{negative}</span>)
+                        </button>
+                    </div>
+                </div>
+                &thinsp;
+            </div>
+            <div
+                className={classes.message}
+                style={{
+                    display: display ? 'block' : 'none',
+                    ...messageStyle
+                }}
+            >
+                {!disable ? (
+                    <Icon.Check className={classes['message-icon']} />
+                ) : (
+                    <Icon.X className={classes['message-icon']} />
+                )}
+                <div>
+                    {!disable ? (
+                        <>
+                            {formatMessage({
+                                id: 'thank',
+                                defaultMessage: 'Thank for your vote!'
+                            })}
+                        </>
+                    ) : (
+                        <>
+                            {formatMessage({
+                                id: 'voted',
+                                defaultMessage: 'You have voted already!'
+                            })}
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Rating
+export default Rating;
--- src/simi/App/nativeInner/Faq/Category/index.js
+++ src/simi/App/nativeInner/Faq/Category/index.js
@@ -3,79 +3,88 @@ import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import SearchBox from '../SearchBox/searchbox.js';
 // import Loader from '../Loader/Loader'
-import Loader from '../../Loader'
+import Loader from '../../Loader';
 
-import CategoriesList from '../FaqsListContainer/CategoriesList'
-import { useCategoryListUrl } from '../../talons/Faq/useCategoryListUrl'
-import { useCategoryList } from '../../talons/Faq/useCategoryList'
+import CategoriesList from '../FaqsListContainer/CategoriesList';
+import { useCategoryListUrl } from '../../talons/Faq/useCategoryListUrl';
+import { useCategoryList } from '../../talons/Faq/useCategoryList';
+import { useIntl } from 'react-intl';
 
 const Category = props => {
-	const { categoryUrl = "" } = useParams();
-	const {
-		categoryData,
-		categoryLoading,
-		categoryErrorMessage
-	} = useCategoryListUrl({url_key: categoryUrl});
-	const { 
-		categoriesData,
-		categoriesLoading,
-		categoriesError 
-	} = useCategoryList()
-	const [userInput, setUserInput] = useState('');
-	const [searchInput, setSearchInput] = useState('');
-	const [categories, setCategories] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [reRender, setReRender] = useState(false)
-	useEffect(() => {
-		if(categoryData) {
-			const _category = categoryData.MpMageplazaFaqsCategoryList.items
-			setCategories(_category)
-		}	
-	}, [reRender])
-	useEffect(() => {
-		setTimeout(() => setReRender(true), 2000)
-	})
-	if(!categoryData || !categoryData.MpMageplazaFaqsCategoryList) {
-		return <Loader />;
-	}
-	if(!categoriesData || !categoriesData.MpMageplazaFaqsCategoryList) {
-		return <Loader />;
-	}
+    const { categoryUrl = '' } = useParams();
+    const { formatMessage } = useIntl();
+    const {
+        categoryData,
+        categoryLoading,
+        categoryErrorMessage
+    } = useCategoryListUrl({ url_key: categoryUrl });
+    const {
+        categoriesData,
+        categoriesLoading,
+        categoriesError
+    } = useCategoryList();
+    const [userInput, setUserInput] = useState('');
+    const [searchInput, setSearchInput] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [reRender, setReRender] = useState(false);
+    useEffect(() => {
+        if (categoryData) {
+            const _category = categoryData.MpMageplazaFaqsCategoryList.items;
+            setCategories(_category);
+        }
+    }, [reRender]);
+    useEffect(() => {
+        setTimeout(() => setReRender(true), 2000);
+    });
+    if (!categoryData || !categoryData.MpMageplazaFaqsCategoryList) {
+        return <Loader />;
+    }
+    if (!categoriesData || !categoriesData.MpMageplazaFaqsCategoryList) {
+        return <Loader />;
+    }
 
-	const onChange = (e) => {
-		setUserInput(e.target.value)
-	}
-	const onSearch = () => {
-		setLoading(true)
-		setTimeout(() => {
-			setLoading(false)
-			if(userInput != '') {
-				setCategories(categoriesData.MpMageplazaFaqsCategoryList.items)
-			} else {
-				setCategories(categoryData.MpMageplazaFaqsCategoryList.items)
-			}
-			setSearchInput(userInput)
-		}, 500)
-	}
-	
-	return (
-		<div className='container'>
-			<Helmet>
+    const onChange = e => {
+        setUserInput(e.target.value);
+    };
+    const onSearch = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+            if (userInput != '') {
+                setCategories(categoriesData.MpMageplazaFaqsCategoryList.items);
+            } else {
+                setCategories(categoryData.MpMageplazaFaqsCategoryList.items);
+            }
+            setSearchInput(userInput);
+        }, 500);
+    };
+
+    return (
+        <div className="container">
+            <Helmet>
                 <meta charSet="utf-8" />
-                <title>{categories.length ? categories[0].name : ''} - Frequently Answer and Question</title>
+                <title>
+                    {categories.length ? categories[0].name : ''}
+                    {formatMessage({
+                        id: 'answerAndQuestion',
+                        defaultMessage: '- Frequently Answer and Question'
+                    })}
+                </title>
             </Helmet>
-			<SearchBox 
-				onChange={onChange}
-				userInput={userInput}
-				onSearch={onSearch}
-			/>
-			<CategoriesList
-				loading={loading}
-				categories={categories}
-				searchInput={searchInput}
-				reRender={reRender}/>
-		</div>
-	)
-}
+            <SearchBox
+                onChange={onChange}
+                userInput={userInput}
+                onSearch={onSearch}
+            />
+            <CategoriesList
+                loading={loading}
+                categories={categories}
+                searchInput={searchInput}
+                reRender={reRender}
+            />
+        </div>
+    );
+};
 
-export default Category
+export default Category;
