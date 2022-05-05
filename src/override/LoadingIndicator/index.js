e609d4bf6a50ebcc5f19db4aa970505a8553459b
CodyNguyen <cody@simicart.com>
2022-05-05 11:54:38 +0700

    Fix warned issues

---
+++ src/override/LoadingIndicator/index.js
@@ -0,0 +1,7 @@
+export {
+    default
+} from '@magento/venia-ui/lib/components/LoadingIndicator/indicator';
+export {
+    default as Spinner
+} from '@magento/venia-ui/lib/components/LoadingIndicator/spinner';
+export { default as fullPageLoadingIndicator } from './static';
