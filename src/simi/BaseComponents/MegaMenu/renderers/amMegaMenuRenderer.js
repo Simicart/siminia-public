9c0863b3b2eb6c374bcb6d5ad8cc19b14482609c
Duy <it.nnduy09@gmail.com>
2022-05-05 14:32:08 +0700

    Update mega menu

--- src/simi/BaseComponents/MegaMenu/renderers/amMegaMenuRenderer.js
+++ src/simi/BaseComponents/MegaMenu/renderers/amMegaMenuRenderer.js
@@ -45,7 +45,6 @@ export default function amMegaMenuRenderer({
   };
 
   const content = ReactHtmlParser(html, options);
-  console.log("content",content)
   const isEmptyContent =
     !Array.isArray(content) || content.every(node => !node);
 
