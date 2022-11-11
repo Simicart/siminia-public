363be3f18fcb89bcab784bd4a3d8def8c540c637
hung <quochung@simicart.com>
2022-11-11 11:47:39 +0700

    update icon

--- src/simi/Helper/Url.js
+++ src/simi/Helper/Url.js
@@ -67,7 +67,7 @@ export const logoUrl = () => {
         );
         return url.href;
     }
-    return 'https://www.simicart.com/skin/frontend/default/simicart2.1/images/simicart/new_logo_small.png';
+    return 'https://gardenhaven.co.uk/media/logo/stores/1/gh.png';
 };
 
 /*
