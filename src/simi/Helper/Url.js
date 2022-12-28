62e650bb7b4d3cd78f6656c5170c7bd41d483684
hung <quochung@simicart.com>
2022-12-28 15:54:29 +0700

    Update logo,favicon, spash

--- src/simi/Helper/Url.js
+++ src/simi/Helper/Url.js
@@ -67,7 +67,7 @@ export const logoUrl = () => {
         );
         return url.href;
     }
-    return 'https://www.simicart.com/skin/frontend/default/simicart2.1/images/simicart/new_logo_small.png';
+    return 'https://nicole-simi.bsscommerce.com/media/logo/stores/1/logo-luma.png';
 };
 
 /*
