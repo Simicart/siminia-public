28d02fc6583745fd9ceeef958f54c60462ce38f3
codynguyen <>
2023-01-16 16:05:35 +0700

    Label config for home page

--- src/simi/talons/TapitaPageBuilder/useProducts.js
+++ src/simi/talons/TapitaPageBuilder/useProducts.js
@@ -1,5 +1,10 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
 import { useQuery } from '@apollo/client';
+const productLabelEnabled =
+    window.SMCONFIGS &&
+    window.SMCONFIGS.plugins &&
+    window.SMCONFIGS.plugins.SM_ENABLE_PRODUCT_LABEL &&
+    parseInt(window.SMCONFIGS.plugins.SM_ENABLE_PRODUCT_LABEL) === 1;
 
 const ItemFragment = gql`
     fragment ItemFragment on ProductInterface {
@@ -11,6 +16,44 @@ const ItemFragment = gql`
             label
             __typename
         }
+                ${
+                    productLabelEnabled
+                        ? `
+                        product_label {
+                            active
+                            apply_outofstock_product
+                            conditions_serialized {
+                              aggregator
+                              attribute
+                              conditions
+                              is_value_processed
+                              operator
+                              type
+                              value
+                            }
+                            created_at
+                            customer_groups
+                            file
+                            id
+                            image_data {
+                              angle
+                              height
+                              heightOrigin
+                              left
+                              top
+                              width
+                              widthOrigin
+                            }
+                            name
+                            priority
+                            store_views
+                            updated_at
+                            valid_end_date
+                            valid_start_date
+                          }
+                `
+                        : ``
+                }
         thumbnail {
             url
             label
