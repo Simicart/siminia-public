b1039d6ac08bb08212b6ec2cc6832eaf5afacdf6
hung <quochung@simicart.com>
2023-01-16 10:49:05 +0700

    update reward point at checkout

--- src/simi/BaseComponents/RewardPoint/components/Cart/apply.js
+++ src/simi/BaseComponents/RewardPoint/components/Cart/apply.js
@@ -11,7 +11,7 @@ import { useApply } from 'src/simi/BaseComponents/RewardPoint/talons/useApply';
 import { Price } from '@magento/peregrine';
 
 const Apply = props => {
-    const { useAccrodion, rewardPoint, refetchCartPage, isCheckout } = props;
+    const { useAccrodion, rewardPoint, refetchCartPage } = props;
 
     const { formatMessage } = useIntl();
     const classes = useStyle(defaultClasses, props.classes);
@@ -20,7 +20,6 @@ const Apply = props => {
         operations: {
             ...customerOperations
         },
-        isCheckout,
         rewardPoint,
         refetchCartPage,
         formatMessage
