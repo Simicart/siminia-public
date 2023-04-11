7f18071ff1324861bfef201677502c54fbfb7f85
Long090501 <96682226+Long090501@users.noreply.github.com>
2023-04-11 08:30:07 +0700

    Fix bug

--- src/abenson-cart/components/HeaderNative.js
+++ src/abenson-cart/components/HeaderNative.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ArrowLeft } from 'react-feather'
+import { useHistory } from 'react-router-dom'
 
 const HeaderAbsNative = () => {
+    const history = useHistory()
+
     return (
         <>
             <div className='abs-header-native'>
                 <h1>Your cart</h1>
-                <ArrowLeft className='abs-back'></ArrowLeft>
+                <ArrowLeft className='abs-back' onClick={() => history.goBack()}></ArrowLeft>
             </div>   
         </>
     )
