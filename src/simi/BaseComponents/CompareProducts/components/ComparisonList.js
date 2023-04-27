a76ce2a2611dd51e4130cc016825d1989cf8a0e3
Long090501 <96682226+Long090501@users.noreply.github.com>
2023-04-27 09:50:26 +0700

    Fix bug

--- src/simi/BaseComponents/CompareProducts/components/ComparisonList.js
+++ src/simi/BaseComponents/CompareProducts/components/ComparisonList.js
@@ -223,24 +223,24 @@ const ComparisonList = () => {
                     ))}
                 </div>
 
-                <div className="cmp-des">
+                {comparisonList.find(element => element.description) && (<div className="cmp-des">
                     <h3>Description</h3>
-                </div>
+                </div>)}
 
-                <div className="cmp-des-list">
+                {comparisonList.find(element => element.description) && (<div className="cmp-des-list">
                     {comparisonList.map((element) => (
                         <div style={{ flex: flexDisplay(), padding: 15 }}>
                             <RichContent html={element.description.html}></RichContent>
                         </div>
                     ))}
-                </div>
+                </div>)}
 
-                {comparisonList.find(element => element.activity !== null) && (
+                {comparisonList.find(element => element.activity) && (
                     <div className="cmp-activity">
                         <h3>Activity</h3>
                     </div>)}
 
-                {comparisonList.find(element => element.activity !== null) && (
+                {comparisonList.find(element => element.activity) && (
                     <div className="cmp-activity-list">
                         {comparisonList.map((element) => (
                             <div style={{ flex: flexDisplay(), padding: 15 }}>
